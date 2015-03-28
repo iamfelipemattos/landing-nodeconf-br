3399cb2944a2e16ed0c8b4d02809a2ef5bf734d1
Felipe Souza <contato@felipesouza.net>
2015-03-28 00:04:52 -0300

    Adiciona novas tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,38 @@
-var gulp               = require('gulp'),
-     jshint              = require('gulp-jshint'),
-     uglify              = require('gulp-uglify'),
-     concat            = require('gulp-concat'),
-     rename           = require('gulp-rename'),
+var gulp              = require('gulp'),
+    // stylus             = require('gulp-stylus');
+     concat            = require('gulp-concat');
+     //filter                = require('gulp-filter');
      browserSync   = require('browser-sync'),
      files                = "./src/*.js";
 
-//Aqui criamos uma nova tarefa através do ´gulp.task´ e damos a ela o nome 'lint'
-gulp.task('lint', function() {
+// Cria task atraves do ´gulp.task´ e é nomeada de  'lint'
+gulp.task('lint', function() {});
 
-// Aqui carregamos os arquivos que a gente quer rodar as tarefas com o `gulp.src`
-// E logo depois usamos o `pipe` para rodar a tarefa `jshint`
-gulp.src(files)
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
-});
+// Cria task dist
+gulp.task('dist', function() {});
 
-//Criamos outra tarefa com o nome 'dist'
-gulp.task('dist', function() {
-
-// Carregamos os arquivos novamente
-// E rodamos uma tarefa para concatenação
-// Renomeamos o arquivo que sera minificado e logo depois o minificamos com o `uglify`
-// E pra terminar usamos o `gulp.dest` para colocar os arquivos concatenados e minificados na pasta build/
-gulp.src(files)
-    .pipe(concat('./dist'))
-    .pipe(rename('dist.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('./dist'));
-});
-
-//Criamos uma tarefa 'default' que vai rodar quando rodamos `gulp` no projeto
+//Cria uma task que iniciara assim que o gulp rodar
 gulp.task('default', function() {
-    // Usamos o `gulp.run` para rodar as tarefas
-    // E usamos o `gulp.watch` para o Gulp esperar mudanças nos arquivos para rodar novamente
     gulp.run('lint', 'dist');
     gulp.watch(files, function(evt) {
         gulp.run('lint', 'dist');
     });
 });
 
+/*
+gulp.task('css', function () {
+    var filter = Filter('"**"/*.styl');
+    return gulp.src([
+            './styles/stylus/**.styl',
+            './styles/css/**.css'
+        ])
+        .pipe(filter)
+        .pipe(stylus())
+        .pipe(filter.restore())
+        .pipe(concat('out.css'))
+        .pipe(gulp.dest('./styles'));
+});
+*/
 // inicia pelo localhost
 gulp.task('browser-sync', function() {
     browserSync.init(['./views/**', '../assets/css/**'], {
@@ -51,5 +44,5 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('watch', ['browser-sync'], function () {
-    gulp.watch('../assets/css/*.css');
+    gulp.watch('./assets/css/*.css');
 });
