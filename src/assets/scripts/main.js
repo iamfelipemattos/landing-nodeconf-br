f60e757b6e4c9b8d80da9b62387d6e11693d75bf
Thiago Augusto Quintanilha <thiago.zeh@hotmail.com>
2015-03-31 18:01:43 -0300

    scrolling animated e ajustes de css

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$('.menu a').click(function(){
+		var id = $(this).prop("hash");
+
+		if(id == '#home'){
+			$('html, body').animate({
+			    scrollTop : 0
+			},500);
+		}else{
+			$('html, body').animate({
+			    scrollTop : $('section'+id+'').offset().top
+			},500);
+		}
+	});
+})
